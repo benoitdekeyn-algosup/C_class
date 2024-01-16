6766cc54c1df297e4f84b57139900c9785fa5c41
Benoît de Keyn <146000855+benoitdekeyn-algosup@users.noreply.github.com>
2024-01-16 14:20:58 +0100

    first leo message and begin of design

---
+++ app/main.c
@@ -0,0 +1,6 @@
+
+
+printf("\n\n____________________ List of Products ________________________\n\n");
+
+printf(" -  ID : %u   Name : %s   Quantity : %u  Price : %f\n", product1.id, product1.name, product1.price, product1.quantity);
+
