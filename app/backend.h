f714dd278117fdb6dfa52f708233f70c5edb96b0
Benoît de Keyn <146000855+benoitdekeyn-algosup@users.noreply.github.com>
2024-01-16 14:39:33 +0100

    fill backend.c

---
+++ app/backend.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct product {
+    unsigned int id;
+    char name[100];
+    unsigned int quantity;
+    float price;
+} product_t;
+
+int read_report(const char* name, char* content);
+int write_report(const char* name, const char* content);
+int add_record(const product_t* product);
+int view_record(const unsigned int id, product_t* product);
+int search_record(); // TODO
+int update_record(const unsigned int id, const product_t* product);
+int delete_record(const unsigned int id);
